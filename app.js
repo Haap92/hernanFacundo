45642d872c606c95d7ae0f56fdbbc4ba22b968b5
vrana99 <fvranaa@gmail.com>
2021-02-25 20:32:56 -0300

    segundo commit-Facundo elmasgrande

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-//Aca se va a ejecutar la aplicacion.
+function elMasGrande (numeroA, numeroB){
+if(numeroA>numeroB){
+    return (numeroA + " es mas grande que " + NumeroB)
+}
+else {
+    return (numeroB + " es mas grande que " + numeroA)
+}
+}
